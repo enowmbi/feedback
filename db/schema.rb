ae649dbce211a0968c5f775e778681ff78d4059d
Enow Mbi <benowmbi@yahoo.com>
2022-05-05 08:36:49 +0100

    Create questions tags

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_05_072420) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_05_073527) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_05_072420) do
     t.index ["title"], name: "index_questions_on_title", unique: true
   end
 
+  create_table "questions_tags", force: :cascade do |t|
+    t.bigint "question_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_questions_tags_on_question_id"
+    t.index ["tag_id"], name: "index_questions_tags_on_tag_id"
+  end
+
   create_table "tags", force: :cascade do |t|
     t.string "name", null: false
     t.text "description", null: false
@@ -30,4 +39,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_05_072420) do
     t.index ["name"], name: "index_tags_on_name", unique: true
   end
 
+  add_foreign_key "questions_tags", "questions"
+  add_foreign_key "questions_tags", "tags"
 end
