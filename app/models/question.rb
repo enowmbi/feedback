ce1838a3525da4e3ae78533405cc59a132297ea1
Enow Mbi <benowmbi@yahoo.com>
2022-05-05 10:28:20 +0100

    Add rich text to questions

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,10 @@
 class Question < ApplicationRecord
+  has_rich_text :body
+
   has_many :comments, as: :commentable
+
   has_many :questions_tags
   has_many :tags, through: :questions_tags
+  
   has_many :answers
 end
