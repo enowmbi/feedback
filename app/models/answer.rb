79f358bd826a560b6af063f4896fec110995a7a0
Enow Mbi <benowmbi@yahoo.com>
2022-05-05 23:29:46 +0100

    Add association between user and answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,5 @@ class Answer < ApplicationRecord
   has_rich_text :body
   belongs_to :question
   has_many :comments, as: :comments
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_05_215110) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_05_222330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,7 +56,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_05_215110) do
     t.bigint "question_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
     t.index ["question_id"], name: "index_answers_on_question_id"
+    t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -123,6 +125,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_05_215110) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "answers", "questions"
+  add_foreign_key "answers", "users"
   add_foreign_key "questions", "users"
   add_foreign_key "questions_tags", "questions"
   add_foreign_key "questions_tags", "tags"
