f6baf82ca6365b78ebcca1f713b162f90a0e601e
Enow Mbi <benowmbi@yahoo.com>
2022-05-05 23:04:38 +0100

    Add devise for user authentication

--- Gemfile
+++ Gemfile
@@ -46,6 +46,10 @@ gem "bootsnap", require: false
 # gem "sassc-rails"
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
+
+# Use devise for user authentication
+gem 'devise'
+
 gem "image_processing", "~> 1.2"
 
 group :development, :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "questions#index"
   resources :answers
   resources :comments
   resources :tags
   resources :questions
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
